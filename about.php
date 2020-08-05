180148924ee265a585b7a8da3768c8a7fd1a677e
Jasper Vries <git@jaspervries.nl>
2020-08-05 22:44:27 +0200

    Docs: about en help aangepast

---
+++ about.php
@@ -0,0 +1,59 @@
+<?php 
+/*
+ 	assetwebsite - viewer en aanvraagformulier voor verkeersmanagementassets
+    Copyright (C) 2020 Gemeente Den Haag, Netherlands
+    Developed by Jasper Vries
+    Modified for Verkeersbordenkaart
+    Copyright (C) 2020 Jasper Vries
+ 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+ 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+ 
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+session_start();
+include_once('getuserdata.inc.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Verkeersbordenkaart - Over</title>
+<link rel="stylesheet" type="text/css" href="bundled/jquery-ui/jquery-ui.min.css">
+<link rel="stylesheet" type="text/css" href="style/style.css">
+<link rel="icon" type="image/png" href="favicon.png">
+<script type="text/javascript" src="bundled/jquery/jquery.min.js"></script>
+<script type="text/javascript" src="bundled/jquery-ui/jquery-ui.min.js"></script>
+<script type="text/javascript" src="help.js"></script>
+</head>
+<body>
+
+<?php
+include('menu.inc.php');
+?>
+
+<div id="content">
+    <h1>Over Verkeersbordenkaart</h1>
+    <p>Verkeersbordenkaart is een grafische interface voor het bekijken van de open data "Verkeersborden" van <a href="https://docs.ndw.nu/api/">https://docs.ndw.nu/api/</a>. Deze dataset bevat informatie en locaties van zo'n drie miljoen RVV-borden uit heel Nederland.</p>
+    <p>Verkeersbordenkaart kopieert de data uit de dataset naar een lokale database en presenteert de inhoud van deze database op een kaartondergrond. De data die gepresenteerd wordt in Verkeersbordenkaart wordt ieder uur geactualiseerd.</p>
+    <p>Verkeersbordenkaart is gemaakt door Jasper Vries. Er wordt geen aansprakelijkheid aanvaard voor compleetheid, juistheid, geschiktheid en correcte werking van deze website.</p>
+    
+    <h2>Toekomstige functionaliteiten</h2>
+    <p>Melding tonen wanneer niet ver genoeg is ingezoomd.</p>
+    <p>RVV-code bij pictogram op kaart tonen.</p>
+    <p>Afbeeldingen van verkeersborden worden direct geladen vanaf de open data server, wanneer deze beschikbaar zijn.</p>
+
+    <h2>Broncode</h2>
+    <p>De broncode van Verkeersbordenkaart is gepubliceerd op <a href="https://github.com/jaspervries/verkeersbordenkaart/">GitHub</a>.</p>
+    <p>Verkeersbordenkaart is gebouwd op de basis van <a href="http://assets.vcdh.nl/">Assetwebsite</a>.
+</div>
+</body>
+</html>
--- help/index.php
+++ help/index.php
@@ -3,6 +3,8 @@
  	assetwebsite - viewer en aanvraagformulier voor verkeersmanagementassets
     Copyright (C) 2016-2020 Gemeente Den Haag, Netherlands
     Developed by Jasper Vries
+    Modified for Verkeersbordenkaart
+    Copyright (C) 2020 Jasper Vries
  
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -19,70 +21,10 @@
 */
 ?>
 <h1>Kaartlagen</h1>
-<p>Verschillende soorten assets zijn ingedeeld in afzonderlijke kaartlagen. Schakel &eacute;&eacute;n of meerder kaartlagen in om assets op de kaart te tonen. Door met de linkermuisknop op een asset op de kaart te klikken wordt een popup met aanvullende informatie geopend, vanuit deze popup kan een venster met detailinformatie worden geopend en kan de locatie worden geopend in Google Street View&trade;. Het venster met detailinformatie kan ook rechtstreeks worden geopend door met de rechtermuisknop op een asset te klikken.</p>
+<p>Schakel de kaartlaag Verkeersborden in om locaties van verkeersborden op de kaart te zien. Zoom indien nodig verder in. Door met de linkermuisknop op een verkeersbord op de kaart te klikken wordt een popup met aanvullende informatie geopend, vanuit deze popup kan een venster met detailinformatie worden geopend en kan de locatie worden geopend in Google Street View&trade;. Het venster met detailinformatie kan ook rechtstreeks worden geopend door met de rechtermuisknop op een verkeersbord te klikken.</p>
 
 <h1>Kaartachtergrond en -weergave</h1>
-<p>Er zijn verschillende kaartachtergronden beschikbaar die naar wens kunnen worden gekozen. Hierbij is het ook mogelijk om de weergave van de kaart aan te passen. De gekozen kaartachtergrond en -weergave worden ook toegepast op de pagina <a href="aanvraagformulier.php">DRIP-aanvraagformulier</a>, maar zijn daar niet te wijzigen.</p>
-
-<h1>Legenda</h1>
-<?php
-include('dbconnect.inc.php');
-$qry = "SELECT `id`, `name` FROM `".$db['prefix']."assettype`
-ORDER BY `name`";
-$res = mysqli_query($db['link'], $qry);
-if (mysqli_num_rows($res)) {
-    echo '<table>';
-    while($data = mysqli_fetch_row($res)) {
-        echo '<tr><td><img src="image.php?t=' . $data[0] . '&amp;w=0" width="16" heigth=16"></td><td>' . htmlspecialchars($data[1]) . '</td></tr>';
-        //TODO: aanname dat DRIP type 1 is en VRI type 3
-        if ($data[0] == 1) {
-            //DRIP met standaardtekst
-            echo '<tr><td><img src="image.php?t=' . $data[0] . '&amp;w=0&amp;i=1" width="16" heigth=16"></td><td>' . htmlspecialchars($data[1]) . ' met standaardtekst</td></tr>';
-        }
-        if ($data[0] == 3) {
-            //iVRI
-            echo '<tr><td><img src="image.php?t=' . $data[0] . '&amp;w=0&amp;i=1" width="16" heigth=16"></td><td>iVRI</td></tr>';
-        }
-    }
-    echo '</table>';
-}
-?>
-
-<h1>Wegbeheerders</h1>
-<p>De kleur van een pictogram geeft aan door welke wegbeheerder een asset wordt aangestuurd/bediend.</p>
-<?php
-include('dbconnect.inc.php');
-$qry = "SELECT `id`, `name` FROM `".$db['prefix']."organisation`
-ORDER BY `name`";
-$res = mysqli_query($db['link'], $qry);
-if (mysqli_num_rows($res)) {
-    echo '<table>';
-    while($data = mysqli_fetch_row($res)) {
-        echo '<tr><td><img src="image.php?t=0&amp;w=' . $data[0] . '" width="16" heigth=16"></td><td>' . htmlspecialchars($data[1]) . '</td></tr>';
-    }
-    echo '</table>';
-}
-?>
-
-
-<!--
-<h1>Zoeken</h1>
-<p>De zoekfunctie kan gebruikt worden om assets en hectometerposities te zoeken. De zoekfunctie is niet hoofdlettergevoelig.</p>
-
-<h2>Hectometerposities</h2>
-<p>Er wordt automatisch gezocht op hectometerposities wanneer een wegnummer (inclusief A/N), optionele rijrichting en hectometerpunt worden opgegeven. De rijrichting kan geschreven worden als <em>Li</em> en <em>Re</em>, maar ook <em>links</em>, <em>rechts</em> en letters van afritten, parallelbanen of verbindingsbogen kunnen gebruikt worden. Wanneer de rijrichting wordt weggelaten, worden alle rijbanen weergegeven. Het hectometerpunt wordt exact ge&iuml;nterpreteerd en mag &eacute;&eacute;n decimaal bevatten. Spaties tussen letters en cijfers mogen weggelaten worden. Format: <span style="font-family: monospace;">&lt;wegnummer&gt; [li|links|re|rechts|a-z] &lt;hectometer&gt; [a-z]</span></p>
-<p>Enkele voorbeelden:</p>
-<ul>
-<li>A4 Li 46,3</li>
-<li>A4 rechts 46.3</li>
-<li>A4re46.3</li>
-<li>A4 f 46.3</li>
-<li>A4 46.3 f</li>
-<li>A4 li f 46.3</li>
-<li>A4f li46.3</li>
-<li>A4li46.3f</li>
-</ul>
--->
+<p>Er zijn verschillende kaartachtergronden beschikbaar die naar wens kunnen worden gekozen. Hierbij is het ook mogelijk om de weergave van de kaart aan te passen. </p>
 
 <h1>Google Street View&trade;</h1>
 <p>Google Street View&trade; kan ook worden geopend door met de rechtermuisknop op de kaart te klikken (niet op een asset). Er opent dan een popup met een link naar Google Street View&trade;. Er is geen check of er daadwerkelijk Street View&trade; op de beoogde locatie beschikbaar is. In dat geval zal Street View&trade; op de dichtstbijzijnde locatie worden geopend, of kan Street View&trade; niet worden geopend en blijft het venster zwart.</p>
