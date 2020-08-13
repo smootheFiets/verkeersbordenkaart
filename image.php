174baa93c915f2bdd2c4c051934a710edb14a8b8
Jasper Vries <git@jaspervries.nl>
2020-08-13 21:45:26 +0200

    Add: aantal ontbrekende afbeeldingen van borden

--- image.php
+++ image.php
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with Verkeersbordenkaart. If not, see <http://www.gnu.org/licenses/>.
 */
 
-$image = 'images/' . preg_replace('/[[:^alnum:]]/i', '', $_GET['i']) . '.png';
+$image = 'images/' . preg_replace('/[^[:alnum:\()]]/i', '', $_GET['i']) . '.png';
 
 
 if (file_exists($image)) {
